d27672612812739f6c44dd83bf7f9aeff5acb124
minhman1504 <18521082@gm.uit.edu.vn>
2023-01-30 14:17:35 +0700

    debug

--- ScriptToRestoreDB.sh
+++ ScriptToRestoreDB.sh
@@ -3,7 +3,7 @@
 # docker exec -i postgres_container pg_restore -h ${{ parameters.PostgreSQLServer }} -U ${{ parameters.PostgreSQLUser }} -d ${{ parameters.DatabaseName }} --clean --create ${{ parameters.BackupFileName }}
 
 docker cp  postgres-backup.dump postgres_container:/postgres-backup.dump
-docker exec -i postgres_container pg_restore -U postgres -d postgres -C postgres-backup.dump
+docker exec -i postgres_container pg_restore -U postgres -d postgres -Ft -C postgres-backup.dump
 
 # delete database
 docker exec -i postgres_container dropdb -U postgres test-database -f
